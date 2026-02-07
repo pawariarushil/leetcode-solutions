97540e3d530daa26528566a638c0bc5796bdb61b
Rushil Pawaria <pawariarushil@gmail.com>
2026-02-07 17:23:44 +0000

    Sync LeetCode submission - Power of Three (cpp)

---
+++ problems/problems/power_of_three/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n<=0){
+            return false;
+        }
+        while(n>1){
+            if(n%3==0){
+                n=n/3;
+                cout<<n;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
