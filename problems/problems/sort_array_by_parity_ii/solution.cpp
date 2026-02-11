ce387af562d10be33b1cfb1963c3ef6f679d4fd1
Rushil Pawaria <pawariarushil@gmail.com>
2026-02-11 16:21:29 +0000

    Sync LeetCode submission - Sort Array By Parity II (cpp)

---
+++ problems/problems/sort_array_by_parity_ii/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& nums) {
+        vector<int>even;
+        vector<int>odd;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%2==0){
+                even.push_back(nums[i]);
+            }
+            else{
+                odd.push_back(nums[i]);
+            }
+        }
+        int k=0;
+        int m=0;
+        for(int i=0;i<nums.size();i++){
+            if(i%2==0){
+                nums[i]=even[k];
+                k++;
+            }
+            else{
+                nums[i]=odd[m];
+                m++;
+            }
+        }
+        return nums;
+    }
+};
